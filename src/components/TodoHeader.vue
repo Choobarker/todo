f98a2f6ad515efe28eeb65d1b466854996dbc519
Zac Mowatt <zac.mowatt@gmail.com>
2017-03-14 20:44:51 +1300

    Add TodoHeader

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-header">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'todo-header',
+  components: {
+  }
+};
+</script>
+
+<style scoped>
+.todo-header {
+  width: 100%;
+  height: 80px;
+  background: #9CCC65;
+}
+</style>
